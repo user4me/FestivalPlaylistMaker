0f8dfe74c7b419bdd73b5afd868a75db0e3401bb
Quentin Marrecau <quentin.marrecau@cgi.com>
2020-01-07 14:22:34 +0100

    Make it works with constants values

--- FestivalLister.py
+++ FestivalLister.py
@@ -4,9 +4,9 @@ import math
 import spotipy
 import spotipy.util as util
 
-SPOTIPY_CLIENT_ID='your-spotify-client-id'
-SPOTIPY_CLIENT_SECRET='your-spotify-client-secret'
-SPOTIPY_REDIRECT_URI='your-app-redirect-url'
+SPOTIPY_CLIENT_ID='SPOTIPY_CLIENT_ID'
+SPOTIPY_CLIENT_SECRET='SPOTIPY_CLIENT_SECRET'
+SPOTIPY_REDIRECT_URI='SPOTIPY_REDIRECT_URI'
 
 errorlog = open('errorlog.txt','w')
 lineupfile = open('lineup.txt','r')
@@ -23,22 +23,22 @@ else:
     sys.exit()
 index = 0
 scope = 'playlist-modify-public'
-token = util.prompt_for_user_token(username, scope, redirect_uri = 'https://example.com/callback/')
+token = util.prompt_for_user_token(username, scope, client_id=SPOTIPY_CLIENT_ID,client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri = SPOTIPY_REDIRECT_URI)
 
 if token:
     sp = spotipy.Spotify(auth=token)
     sp.trace = False
 
     for x in range(0,len(lineup)):
-        results = sp.search(q=str(lineup[x]), limit=5)
+        results = sp.search(q=str(lineup[x]), limit=3)
         for i, t in enumerate(results['tracks']['items']):
             tracks.append(str(t['id'].strip( 'u' )))
-            print("adding ",t['id'],t['name'])
+            print("adding ",str(t['id'].strip( 'u' )),t['name'])
     while tracks:
         print "adding song",index
         index = index + 1
         try:
-            results = sp.user_playlist_add_tracks(username, playlist_id, tracks[:1], position=None)
+			results = sp.user_playlist_add_tracks(username, playlist_id, tracks[:1], position=None)
         except:
             print "error"
         tracks = tracks[1:]
